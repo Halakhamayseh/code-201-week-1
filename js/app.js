45f558ae2ffbc708ac25f383ec4df83606b62b0d
Halakhamayseh <khamaysehhala95@yahoo.com>
2021-03-04 21:39:37 +0200

    uploding and edit JS

--- js/app.js
+++ js/app.js
@@ -2,60 +2,60 @@
 let counter=0;
 confirm('Lets see how well you know me\n answer these simple questions');
 let userName=prompt('What is your name dear');
+let answers;
 function greetings(name){
   return alert('greetings ' + name +' hope you are well today');
 }
 greetings(userName);
-let answ=prompt('ans with y or no');
+alert('answer with y or no');
 function birthGuess(answ){
-  answ=prompt('Were I born in Zaqra');
+  answ=prompt('Were I born in Zaqra').toLowerCase();
   if(answ==='y' || answ==='yes'){
     alert('correct good job');
     counter+=1;
     // console.log('correct good job')  
-}else {
-  alert('wrong answer');
-}
-answ=prompt('Am I 24 years old');
+  }else {
+    alert('wrong answer');
+  }
+  answ=prompt('Am I 24 years old').toLowerCase();
   if(answ==='y' || answ==='yes'){
     alert('correct good job');
     counter+=1;
   }else {
     alert('wrong answer');
   }
-  answ=prompt('Am I a short guy');
+  answ=prompt('Am I a short guy').toLowerCase();
   if(answ==='y' || answ==='yes'){
     alert('correct good job');
     counter+=1;
   }else {
     alert('wrong answer');
   }
-  answ=prompt('Am I smart');
+  answ=prompt('Am I smart').toLowerCase();
   if(answ==='y' || answ==='yes'){
     alert('correct good job');
     counter+=1;
   }else {
     alert('wrong answer');
   }
-  answ=prompt('Am I into quantum phyics');
+  answ=prompt('Am I into quantum phyics').toLowerCase();
   if(answ==='y' || answ==='yes'){
     alert('correct good job');
     counter+=1;
   }else {
     alert('wrong answer');
   }
-
   return alert('you got '+ counter +' out of 5');
 }
-birthGuess(answ);
+birthGuess(answers);
 let randomNumber = Math.floor(Math.random() * 101);
-let x ;
+let x;
 let userHint=['too high', 'too low', 'out of attempts'];
 let userNumber;
 function randomFxn (b){
   alert('guess a number between \n 0-100');
-let guessCount=4;
-for (let i =0;i<4;i++){
+  let guessCount=4;
+  for (let i =0;i<4;i++){
     x=parseInt(prompt(' guess a number between \n 0-100' ));
     if(x<randomNumber){
       alert(userHint[1]);
@@ -73,22 +73,27 @@ for (let i =0;i<4;i++){
     }
     guessCount--;
     alert('trial left : ' + guessCount);
-}
+  }
 }
 randomFxn(x);
-let possibleCauses=['neutron energy', 'type of reaction', 'target energy '];
+let possibleCauses=['neutron energy', 'type of reaction', 'target energy'];
 let userGuess;
+let trials;
 function nuclearCauses(cross){
-for (let i =1;i<=6;i++){
-  userGuess=prompt('gues a factor that affects the probability of \n nuclear rxn');
-  for (let j =0;j<3;j++){
-if (userGuess===possibleCauses[j]) {
-  j=6;
-  alert('good guess '+possibleCauses+' are all correct')
-  break;
-}
+  for (let i =1;i<=6;i++){
+    cross=prompt('guess a factor that affects the probability of \n nuclear rxn');
+    for (let j =0;j<3;j++){
+      trials=i;
+      if (cross===possibleCauses[j]) {
+        j=6;
+        counter+=1;
+        i=7;
+        alert('good guess '+possibleCauses+' are all correct');
+        break;
+      }
+    }
+    alert('trial sofar : ' + trials);
   }
-  alert('trial sofar : ' + i);
-}
 }
 nuclearCauses(userGuess);
+alert('you got '+ counter +' out of 7');
